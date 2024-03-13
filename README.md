817ae66c88b37dc850ba8f277c5ab3ed8a6d12c0
pnaruka <pankajnaruka111@gmail.com>
2024-03-13 18:09:57 +0530

    documentation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# broadcast_chat
-group chat app
+Broadcast Chat
+
+Description:
+This is a simple application to broadcast messages from a user to all other connected users.
+
+
+Installation:
+To install the project, follow these steps:
+
+Clone the repository: git clone https://github.com/pnaruka/broadcast_chat
+Navigate to the project directory: cd broadcast_chat
+Install dependencies: npm install
+
+Usage:
+After installation, you can use the project as follows:
+
+Run the project: npm start
+Access the application through your web browser at http://localhost:3333
+(You can change the port in index.js file)
+
+Contributing:
+Contributions are welcome! Here's how you can contribute:
+Fork the repository
+Create a new branch: git checkout -b feature
+Make your changes and commit them: git commit -am 'Add new feature'
+Push to the branch: git push origin feature
+Submit a pull request
+
+Additional Information
+For additional information or support, please contact the project maintainers at pankajnaruka111@gmail.com.
--- index.js
+++ index.js
@@ -3,25 +3,30 @@ const http = require("http");
 const { Server } = require("socket.io");
 const path = require('path');
 
+//create express app and set 'public' directory to serve static pages
 const app = express();
 app.use(express.static(path.resolve('./public')));
-const server = http.createServer(app);
 
+//create http and socket server
+const server = http.createServer(app);
 const io = new Server(server);
 
+//create a socket connection whenever a new user connects
 io.on("connection", (socket) => {
-    //console.log(`new user connected ${socket.id}`);
+
+    //when a message is received from a user broadcast it to others
     socket.on("send", (data) => {
         socket.broadcast.emit("broadcast", data);
     })
 })
 
+//define the home api, returning the index.html file
 app.get('/', (req, res) => {
     return res.sendFile('/public/index.html');
 })
 
+//get the port and start the server
 const port = process.env.PORT || 3333;
-
 server.listen(port,"0.0.0.0", () => {
-    console.log(`Server up and listening on ${port}`);
+    console.log("Server up");
 });
