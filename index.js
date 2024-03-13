3b26ffcc33edec0c4f37ced6f425cc0170a539ef
pnaruka <pankajnaruka111@gmail.com>
2024-03-13 12:16:28 +0530

    host edit

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3333;
 
-server.listen(port, () => {
+server.listen(port,"0.0.0.0", () => {
     console.log(`Server up and listening on ${port}`);
 });
